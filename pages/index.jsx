753a81013ad309cf6532ec71eada6f86afd0aeb2
daleinen7 <daleinen@gmail.com>
2020-12-07 11:00:58 -0600

    style homepage to match layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,28 +1,28 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout>
       <Head>
         <title>Frame-E</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           Frame-E: the online frameshop management software
         </h1>
 
-        <p className={styles.description}>
+        <p>
           Get started by <Link href={'/writeorder'}><a className={'btn'}>Writing an Order</a></Link>
         </p>
       </main>
 
-      <footer className={styles.footer}>
+      <footer>
         frame-e custom order writing software for Masterpiece Framing
       </footer>
-    </div>
+    </Layout>
   )
 }
